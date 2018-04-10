f7edded6f5139451ec1a286670aeabc60414ed17
Seren <cmingxuan94@gmail.com>
2018-04-10 19:58:30 +0800

    auto update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const timeout=300  //Change the time duration where you cannot use dismount skills after mounting.
 
 module.exports = function stopdismount(dispatch){
-	let time
+	let time = 0
 	
 	dispatch.hook('C_START_SKILL', 'raw', () => {		
 		if(Date.now()-time < timeout) return false
